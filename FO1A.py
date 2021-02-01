8f30afd2ef65b8a2859470fb810c8ce96c1a6143
MriLevi <74299880+MriLevi@users.noreply.github.com>
2021-02-01 14:39:31 +0100

    fibonnaci mooier gemaakt

--- FO1A.py
+++ FO1A.py
@@ -37,7 +37,6 @@ def opdracht1whileanderekant():
         print(f'{"* " * i}')
         i += 1
 
-opdracht1whileanderekant()
 #opdracht 1 met 1 for loop?
 def opdracht1eenforloop(lengte):
     count=lengte
@@ -53,47 +52,53 @@ def opdracht1eenforloop(lengte):
 def tekstcheck():
     input1=input('Geef een string:')
     input2=input('Geef een string:')
-    for i in range(len(min(input1, input2))):
-        if input1[i] != input2[i]:
-            return print(f'Het eerste verschil zit op index: {i}')
+    for i in range(len(min(input1, input2))): #loop over de hoeveelheid characters in de kortste string
+        if input1[i] != input2[i]: #als er binnen de kortste string al een verschil is met de langere string
+            return print(f'Het eerste verschil zit op index: {i}') #return de index
         else:
-            return print(f'Het eerste verschil zit op index: {min(len(input1), len(input2))}')
+            return print(f'Het eerste verschil zit op index: {min(len(input1), len(input2))}') #return index van eerste "langere" character
 
 ## OPDRACHT 3##
 
 ## A ##
 
 def count(x, lst):
+    #telt hoe vaak x voorkomt in lst
     count = 0
     for i in lst:
         if i == x:
-            count +=1
+            count += 1
     return count
 
 ## B ##
 def neighbourdiff(lst):
+    #controleert het grootste verschil tussen naast elkaar gelegen waardes in lst
     biggestdiff = 0
-    for i in range(len(lst)-1):
+    for i in range(0, len(lst)-1):
         if lst[i+1]-lst[i]>biggestdiff:
             biggestdiff = lst[i+1]-lst[i]
     return biggestdiff
 
 ## C ##
 def eisenchecker(lst):
-     return count(1, lst) > count(0, lst) and count(0, lst) <= 12
+    #controleert of een lijst meer enen dan nullen heeft en of de lijst niet meer dan 12 nullen heeft
+    return count(1, lst) > count(0, lst) and count(0, lst) <= 12
 
 ## OPDRACHT 4 ##
 ##omdat ik niet zeker wist of splicing telde als een bibliotheek functie, heb ik drie versies gemaakt
 ##aptly named...
 
 def biebfunctiepalindroomchecker(str):
+    #checkt op palindromen met reversed method
     return str == ''.join(reversed(str))
 
 
 def palindroomchecker(str):
+    #checkt op palindromen door str te splicen in negatieve richting
     return str == str[::-1]
 
 def handmatigreversepalindroomchecker(str):
+    #draait de string om met een while loop
     reversestr = ''
     index = len(str)
     while index:
@@ -103,6 +108,7 @@ def handmatigreversepalindroomchecker(str):
 
 ## OPDRACHT 5 ##
 def mergesort(lst):
+    #merge sort hakt een lijst
     if len(lst)>1:
         mid = len(lst)//2
         left = lst[:mid]
@@ -110,23 +116,23 @@ def mergesort(lst):
 
         mergesort(left)
         mergesort(right)
-        i=j=k=0
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                lst[k]=left[i]
-                i+=1
+        linkercounter=rechtercounter=counter=0
+        while linkercounter < len(left) and rechtercounter < len(right):
+            if left[linkercounter] < right[rechtercounter]:
+                lst[rechtercounter]=left[linkercounter]
+                linkercounter+=1
             else:
-                lst[k]=right[j]
-                j+=1
-            k+=1
-        while i < len(left):
-            lst[k]=left[i]
-            i+=1
-            k+=1
-        while j < len(right):
-            lst[k]=right[j]
-            j+=1
-            k+=1
+                lst[rechtercounter]=right[counter]
+                counter+=1
+            counter+=1
+        while linkercounter < len(left):
+            lst[rechtercounter]=left[linkercounter]
+            linkercounter+=1
+            rechtercounter+=1
+        while counter < len(right):
+            lst[rechtercounter]=right[j]
+            counter+=1
+            rechtercounter+=1
         return lst
     else: # als de lijst 1 element bevat (of 0), dan is hij al gesorteerd
         return lst
@@ -134,6 +140,12 @@ def mergesort(lst):
 ## OPDRACHT 6 ##
 def gemiddelde(lst):
     return sum(lst) // len(lst)
+
+def gemiddeldelijsten(lst):
+    ans = []
+    for i in lst:
+        ans.append(sum(i) // len(i))
+    return ans
 
 ## OPDRACHT 7 ##
 def randomraden():
@@ -162,7 +174,9 @@ def cyclic_binary_shift(ch, n):
 
 ## OPDRACHT 10 ##
 def fibonaccifinder(n, a=0, b=1):
-    return fibonaccifinder(n-1, b, a+b) if n > 1 else (a, b)[n]
+    if n > 1:
+        return fibonaccifinder(n-1, b, a+b)
+    return (a, b)[n]
 
 
 ## OPDRACHT 11 ##
